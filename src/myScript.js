36f797b10056a65dd34916570af5fca924917dfc
Srilekhini Kadari <srilekhinik@gmail.com>
2019-09-30 14:46:40 +0100

    added handleCharacterKey() function

--- src/myScript.js
+++ src/myScript.js
@@ -18,5 +18,24 @@ function handleLetterKey(key) {
 	}
 }
 
+function handleCharacterKey(key) {
+	var string = key.target.textContent.toString();
+	var array = string.split(' ');  
+	
+	if(shiftKey == true){
+		var character = array[0].toString();
+		document.getElementById("keyboard-display").value = state.previousValue.toString() + character;
+		state.previousValue = document.getElementById("keyboard-display").value;  
+	}
+	else {
+		var character = array[0].toString();
+		document.getElementById("keyboard-display").value = state.previousValue.toString() + character;
+		state.previousValue = document.getElementById("keyboard-display").value; 
+	}
+}
+
 Array.prototype.slice.call(document.getElementsByClassName("letter")).map(eachKey => 
-	eachKey.addEventListener("click", handleLetterKey));
+    eachKey.addEventListener("click", handleLetterKey));
+    
+Array.prototype.slice.call(document.getElementsByClassName("character")).map(eachKey => 
+    eachKey.addEventListener("click", handleCharacterKey));
