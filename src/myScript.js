5e0bfef12d657d72f29dfc80295491d7a30d427e
Srilekhini Kadari <srilekhinik@gmail.com>
2019-09-30 14:45:11 +0100

    added handleLetterKey() function

--- src/myScript.js
+++ src/myScript.js
@@ -1,3 +1,22 @@
 const state = {
 	previousValue: ''
 };
+
+function handleLetterKey(key) {
+	var string = key.target.textContent.toString();
+	var array = string.split(' ');  
+	
+	if(shiftKey == true){
+		var character = array[0].toString().toUpperCase();
+		document.getElementById("keyboard-display").value = state.previousValue.toString() + character;
+		state.previousValue = document.getElementById("keyboard-display").value;  
+	}
+	else {
+		var character = array[0].toString().toLowerCase();
+		document.getElementById("keyboard-display").value = state.previousValue.toString() + character;
+		state.previousValue = document.getElementById("keyboard-display").value; 
+	}
+}
+
+Array.prototype.slice.call(document.getElementsByClassName("letter")).map(eachKey => 
+	eachKey.addEventListener("click", handleLetterKey));
